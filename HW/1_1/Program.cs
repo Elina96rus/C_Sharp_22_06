3b6c350781cce90be093db006901b5b5a6a95ba4
элина ахметханова <Lina>
2023-07-06 15:22:41 +0500

    HW 1_1

---
+++ HW/1_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход три числа
+// и выдает максимальное из них
+
+Console.WriteLine("Введите ваши числa");
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+if(a > b&&a > c){
+    Console.Write("Максимальное число " + $"{a}" );
+}
+if(b > a&&b > c){
+    Console.Write("Максимальное число " + $"{b} ");
+}
+if(c > a&&c > b){
+    Console.Write("Максимальное число " + $"{c} ");
+}
+else if(c==a&&c==b){
+    Console.Write("Числа равны");
+}
