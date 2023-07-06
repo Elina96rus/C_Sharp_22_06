814313f3b1b2d36a9dfa80302e59137fa65b7b56
элина ахметханова <Lina>
2023-07-06 13:15:20 +0500

    Classwork 1_0

---
+++ Lesson_01/1_0/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное само на себя).
+// Например
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.WriteLine("Write a number: ");
+string num_str = Console.ReadLine()!;
+int num = int.Parse(num_str);
+Console.WriteLine(num * num);
