7bb007225795b7388a5f97546551b18d1c21cce5
элина ахметханова <Lina>
2023-07-10 18:21:36 +0500

    Classwork 2_2

---
+++ Lesson_02/2_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит случайное число из открезка [10,99]
+// и показывает наибольшую цифру числа
+//78->8
+
+int  Max (int num){
+    if (num/10 > num %10 )
+    return num/10;
+    return num % 10;
+}
+int RNum = new Random().Next (10,100);
+Console.WriteLine("" + RNum);
+int result = Max(RNum);
+Console.WriteLine("" + result);
