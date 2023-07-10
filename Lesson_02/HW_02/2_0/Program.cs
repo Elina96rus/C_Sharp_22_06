ffaf66ec83ebefecfa60a246f3f7f2bac96f1093
элина ахметханова <Lina>
2023-07-10 18:41:19 +0500

    HW2_0

---
+++ Lesson_02/HW_02/2_0/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и 
+// На выходе показывает вторую цифрц этого числа
+
+string GetNum(int num){
+    if((-1000 < num && num < -100) || (num < 1000 && num > 100)){
+        return $"{num / 10 % 10}";
+    }
+    return "Число не трехзначное";
+}
+Console.WriteLine("Введите ваше число: ");
+Console.WriteLine(GetNum(int.Parse(Console.ReadLine()!)));
