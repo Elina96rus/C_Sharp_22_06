05b7a3541241e7279e4d72aaaeb6b270eda5759c
элина ахметханова <Lina>
2023-07-10 18:37:30 +0500

    Classwork 2_4

---
+++ Lesson_02/2_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет,
+// кратно ли одновременно 7 и 23
+
+void Crat(int num){
+    if (num % 7 ==0 && num % 23 == 0){
+        Console.WriteLine("Да");
+    }
+    else{
+        Console.WriteLine("Нет");
+    }
+}
+int numA = int.Parse(Console.ReadLine()!);
+Crat(numA);
